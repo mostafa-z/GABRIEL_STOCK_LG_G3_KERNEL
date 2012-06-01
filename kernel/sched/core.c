b17b3612bb14821cf85f97dd19ef26e77069a222
Al Viro <viro@zeniv.linux.org.uk>
2012-06-01 14:22:01 -0400

    sanitize tsk_is_polling()
    Make default just return 0.  The current default (checking
    TIF_POLLING_NRFLAG) is taken to architectures that need it;
    ones that don't do polling in their idle threads don't need
    to defined TIF_POLLING_NRFLAG at all.
    
    ia64 defined both TS_POLLING (used by its tsk_is_polling())
    and TIF_POLLING_NRFLAG (not used at all).  Killed the latter...
    
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -557,7 +557,7 @@ static bool set_nr_and_not_polling(struct task_struct *p)
 #ifdef CONFIG_SMP
 
 #ifndef tsk_is_polling
-#define tsk_is_polling(t) test_tsk_thread_flag(t, TIF_POLLING_NRFLAG)
+#define tsk_is_polling(t) 0
 #endif
 
 void resched_task(struct task_struct *p)
