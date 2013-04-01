014c7de2ff0a3b1e3779db18fef26c194088bef8
Rob Herring <rob.herring@calxeda.com>
2013-04-01 13:53:38 -0500

    ARM: make sched_clock just call a function pointer
    This converts sched_clock to simply a call to a function pointer in order
    to allow overriding it. This will allow for use with 64-bit counters where
    overflow handling is not needed.
    
    Signed-off-by: Rob Herring <rob.herring@calxeda.com>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/sched_clock.h
+++ include/linux/sched_clock.h
@@ -17,5 +17,7 @@ static inline void sched_clock_postinit(void) { }
 extern void sched_clock_register(u64 (*read)(void), int bits,
 				 unsigned long rate);
 
+extern unsigned long long (*sched_clock_func)(void);
+
 #endif
 
