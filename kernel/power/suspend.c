c6898430afe8ea755fbd6d120c3508c74767d16f
Brandt, Todd E <todd.e.brandt@intel.com>
2013-07-11 07:44:35 +0000

    PM / Sleep: increase ftrace coverage in suspend/resume
    Change where ftrace is disabled and re-enabled during system
    suspend/resume to allow tracing of device driver pm callbacks.
    Ftrace will now be turned off when suspend reaches
    disable_nonboot_cpus() instead of at the very beginning of system
    suspend.
    
    Ftrace was disabled during suspend/resume back in 2008 by
    Steven Rostedt as he discovered there was a conflict in the
    enable_nonboot_cpus() call (see commit f42ac38 "ftrace: disable
    tracing for suspend to ram").  This change preserves his fix by
    disabling ftrace, but only at the function where it is known
    to cause problems.
    
    The new change allows tracing of the device level code for better
    debug.
    
    [rjw: Changelog]
    Signed-off-by: Todd Brandt <todd.e.brandt@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    
    (cherry picked from commit 6b18afec521f7895e637ac3ccaf7ce56346df976)

--- kernel/power/suspend.c
+++ kernel/power/suspend.c
@@ -213,6 +213,7 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 		goto Platform_wake;
 	}
 
+	ftrace_stop();
 	error = disable_nonboot_cpus();
 	if (error || suspend_test(TEST_CPUS))
 		goto Enable_cpus;
@@ -235,6 +236,7 @@ static int suspend_enter(suspend_state_t state, bool *wakeup)
 
  Enable_cpus:
 	enable_nonboot_cpus();
+	ftrace_start();
 
  Platform_wake:
 	if (need_suspend_ops(state) && suspend_ops->wake)
