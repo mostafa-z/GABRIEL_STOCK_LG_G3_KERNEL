cbfe54a85a9dbfc1a0305d8512c33363c417007d
Naveen Ramaraj <nramaraj@codeaurora.org>
2013-09-12 12:07:00 -0700

    mmc: sdhci: Add a mechanism to disable HPI
    Certain cards might cause a timeout when used with HPI mode
    due to card specific issues. Provide a mechanism where HPI
    mode can be turned OFF to workaround such issues.
    
    Signed-off-by: Naveen Ramaraj <nramaraj@codeaurora.org>

--- drivers/mmc/host/sdhci-msm.c
+++ drivers/mmc/host/sdhci-msm.c
@@ -2843,6 +2843,23 @@ static void sdhci_enable_one_MID_mode(struct sdhci_msm_host *msm_host, struct sd
 }
 #endif
 
+#ifdef CONFIG_MMC_SDHCI_MSM_DISABLE_HPI
+static void populate_hpi_mode(struct platform_device *pdev,
+					struct sdhci_msm_host *msm_host)
+{
+	dev_dbg(&pdev->dev,"%s: Disabling HPI mode\n", __func__);
+	return;
+}
+#else
+static void populate_hpi_mode(struct platform_device *pdev,
+					struct sdhci_msm_host *msm_host)
+{
+	msm_host->mmc->caps2 |= MMC_CAP2_STOP_REQUEST;
+	dev_dbg(&pdev->dev,"%s: Enabling HPI mode\n", __func__);
+	return;
+}
+#endif
+
 static int __devinit sdhci_msm_probe(struct platform_device *pdev)
 {
 	struct sdhci_host *host;
@@ -3131,7 +3148,6 @@ static int __devinit sdhci_msm_probe(struct platform_device *pdev)
 #ifndef CONFIG_MACH_LGE
 	msm_host->mmc->caps2 |= MMC_CAP2_CLK_SCALE;
 #endif
-	msm_host->mmc->caps2 |= MMC_CAP2_STOP_REQUEST;
 	msm_host->mmc->caps2 |= MMC_CAP2_ASYNC_SDIO_IRQ_4BIT_MODE;
 	msm_host->mmc->caps2 |= MMC_CAP2_CORE_PM;
 #ifdef CONFIG_MACH_LGE
@@ -3145,6 +3161,8 @@ static int __devinit sdhci_msm_probe(struct platform_device *pdev)
 #endif
 #endif
 	msm_host->mmc->pm_caps |= MMC_PM_KEEP_POWER | MMC_PM_WAKE_SDIO_IRQ;
+
+	populate_hpi_mode(pdev, msm_host);
 
 	if (msm_host->pdata->nonremovable)
 		msm_host->mmc->caps |= MMC_CAP_NONREMOVABLE;
