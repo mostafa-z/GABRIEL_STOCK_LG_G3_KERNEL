eec097502bf2469b3cdd00be4ffd047641eebc07
Viresh Kumar <viresh.kumar@linaro.org>
2013-10-03 20:27:55 +0530

    cpufreq: define generic .attr, .exit() and .verify() routines
    Most of the CPUFreq drivers do similar things in .exit() and .verify() routines
    and .attr. So its better if we have generic routines for them which can be used
    by cpufreq drivers then.
    
    This patch introduces generic .attr, .exit() and .verify() cpufreq drivers.
    
    Signed-off-by: Viresh Kumar <viresh.kumar@linaro.org>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Git-commit: 184345129c53e76069c209f9912ed7c457eceb31
    Git-repo: git://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git
    Signed-off-by: Junjie Wu <junjiew@codeaurora.org>
    Signed-off-by: mostafa-z <mostafazarghami@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -555,6 +555,7 @@ int cpufreq_frequency_table_cpuinfo(struct cpufreq_policy *policy,
 
 int cpufreq_frequency_table_verify(struct cpufreq_policy *policy,
 				   struct cpufreq_frequency_table *table);
+int cpufreq_generic_frequency_table_verify(struct cpufreq_policy *policy);
 
 int cpufreq_frequency_table_target(struct cpufreq_policy *policy,
 				   struct cpufreq_frequency_table *table,
@@ -569,7 +570,7 @@ void cpufreq_cpu_put(struct cpufreq_policy *policy);
 
 /* the following are really really optional */
 extern struct freq_attr cpufreq_freq_attr_scaling_available_freqs;
-
+extern struct freq_attr *cpufreq_generic_attr[];
 void cpufreq_frequency_table_get_attr(struct cpufreq_frequency_table *table,
 				      unsigned int cpu);
 void cpufreq_frequency_table_update_policy_cpu(struct cpufreq_policy *policy);
@@ -577,6 +578,12 @@ void cpufreq_frequency_table_update_policy_cpu(struct cpufreq_policy *policy);
 void cpufreq_frequency_table_put_attr(unsigned int cpu);
 int cpufreq_table_validate_and_show(struct cpufreq_policy *policy,
 				      struct cpufreq_frequency_table *table);
+
+static inline int cpufreq_generic_exit(struct cpufreq_policy *policy)
+{
+	cpufreq_frequency_table_put_attr(policy->cpu);
+	return 0;
+}
 
 ssize_t cpufreq_show_cpus(const struct cpumask *mask, char *buf);
 
