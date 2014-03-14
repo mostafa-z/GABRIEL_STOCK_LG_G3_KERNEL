9e4f370bfce014a2065b6abcef24ed8bca4edb75
Rohit Gupta <rohgup@codeaurora.org>
2014-03-13 20:54:47 -0700

    sched: Call the notify_on_migrate notifier chain for wakeups as well
    Add a change to send notify_on_migrate hints on wakeups of
    foreground tasks from scheduler if their load is above
    wakeup_load_thresholds (default value is 60).
    These hints can be used to choose an appropriate CPU frequency
    corresponding to the load of the task being woken up.
    
    Signed-off-by: Rohit Gupta <rohgup@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2179,6 +2179,7 @@ extern unsigned int sysctl_sched_wakeup_granularity;
 extern unsigned int sysctl_sched_child_runs_first;
 extern unsigned int sysctl_sched_wake_to_idle;
 extern unsigned int sysctl_sched_ravg_window;
+extern unsigned int sysctl_sched_wakeup_load_threshold;
 
 int sched_rt_handler(struct ctl_table *table, int write,
 		void __user *buffer, size_t *lenp,
