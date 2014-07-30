fc2a193594c74761bab4decbd19f0cec8797a2a5
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-07-30 03:27:13 -0700

    sched: window-stats: Allow acct_wait_time to be tuned
    Add sysctl interface to tune sched_acct_wait_time variable at runtime
    
    Change-Id: I38339cdb388a507019e429709a7c28e80b5b3585
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -35,6 +35,7 @@ extern unsigned int sysctl_sched_latency;
 extern unsigned int sysctl_sched_min_granularity;
 extern unsigned int sysctl_sched_wakeup_granularity;
 extern unsigned int sysctl_sched_child_runs_first;
+extern unsigned int sysctl_sched_account_wait_time;
 
 #ifdef CONFIG_SCHED_DEBUG
 extern __read_mostly unsigned int sysctl_sched_yield_sleep_duration;
