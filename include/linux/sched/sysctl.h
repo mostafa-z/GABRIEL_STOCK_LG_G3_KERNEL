4d461dbf32a6fd7da49f8d405997b8fe971083ae
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-08-11 10:21:40 +0530

    sched: window-stats: legacy mode
    Support legacy mode, which results in busy time being seen by governor
    that is close to what it would have seen via existing APIs i.e
    get_cpu_idle_time_us(), get_cpu_iowait_time_us() and
    get_cpu_idle_time_jiffy(). In particular, legacy mode means that only
    task execution time is counted in rq->curr_runnable_sum and
    rq->prev_runnable_sum. Also task migration does not result in
    adjustment of those counters.
    
    Change-Id: If374ccc084aa73f77374b6b3ab4cd0a4ca7b8c90
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- include/linux/sched/sysctl.h
+++ include/linux/sched/sysctl.h
@@ -37,6 +37,7 @@ extern unsigned int sysctl_sched_wakeup_granularity;
 extern unsigned int sysctl_sched_child_runs_first;
 extern unsigned int sysctl_sched_account_wait_time;
 extern unsigned int sysctl_sched_ravg_hist_size;
+extern unsigned int sysctl_sched_freq_legacy_mode;
 
 #ifdef CONFIG_SCHED_DEBUG
 extern __read_mostly unsigned int sysctl_sched_yield_sleep_duration;
