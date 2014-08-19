12aae2e5dc4739ae01eede3aa0b9fe17a6bb7a24
Srivatsa Vaddagiri <vatsa@codeaurora.org>
2014-08-19 12:31:54 +0530

    sched: window_stats: Add "disable" mode support
    "disabled" mode (sched_disble_window_stats = 1) disables all
    window-stats related activity. This is useful when changing key
    configuration variables associated with window-stats feature (like
    policy or window size).
    
    Change-Id: I9e55c9eb7f7e3b1b646079c3aa338db6259a9cfe
    Signed-off-by: Srivatsa Vaddagiri <vatsa@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -1833,7 +1833,7 @@ done:
 
 void inc_nr_big_small_task(struct rq *rq, struct task_struct *p)
 {
-	if (!sched_enable_hmp)
+	if (!sched_enable_hmp || sched_disable_window_stats)
 		return;
 
 	if (is_big_task(p))
@@ -1844,7 +1844,7 @@ void inc_nr_big_small_task(struct rq *rq, struct task_struct *p)
 
 void dec_nr_big_small_task(struct rq *rq, struct task_struct *p)
 {
-	if (!sched_enable_hmp)
+	if (!sched_enable_hmp || sched_disable_window_stats)
 		return;
 
 	if (is_big_task(p))
