1d7d290a95d636f455d1c648d5e77e8cab0bd055
Dorimanx <yuri@bynet.co.il>
2014-09-30 17:39:17 +0300

    Revert "genirq: Allow forcing cpu affinity of interrupts"
    This reverts commit 6a22ce028fa013564e645eb4fa8d639b2320017b.
    
    *Seems to drain more power on IDLE, for now reverting.
    
    Thanks @neobuddy89 for finding it to be the BUG.

--- include/linux/irq.h
+++ include/linux/irq.h
@@ -389,8 +389,7 @@ extern void remove_percpu_irq(unsigned int irq, struct irqaction *act);
 
 extern void irq_cpu_online(void);
 extern void irq_cpu_offline(void);
-extern int irq_set_affinity_locked(struct irq_data *data,
-				   const struct cpumask *cpumask, bool force);
+extern int __irq_set_affinity_locked(struct irq_data *data,  const struct cpumask *cpumask);
 
 #if defined(CONFIG_SMP) && defined(CONFIG_GENERIC_PENDING_IRQ)
 void irq_move_irq(struct irq_data *data);
