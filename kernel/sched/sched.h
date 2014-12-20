ed86d89293b70e8103030573653b849f60096813
Olav Haugan <ohaugan@codeaurora.org>
2014-12-19 16:13:58 -0800

    sched: Add sysctl to enable power aware scheduling
    Add sysctl to enable energy awareness at runtime. This is useful for
    performance/power tuning/measurements and debugging. In addition this
    will match up with the Documentation/scheduler/sched-hmp.txt documentation.
    
    Change-Id: I0a9185498640d66917b38bf5d55f6c59fc60ad5c
    Signed-off-by: Olav Haugan <ohaugan@codeaurora.org>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1062,7 +1062,6 @@ static inline void clear_reserved(int cpu)
 	clear_bit(CPU_RESERVED, &rq->hmp_flags);
 }
 
-extern unsigned int sched_enable_power_aware;
 
 int mostly_idle_cpu(int cpu);
 extern void check_for_migration(struct rq *rq, struct task_struct *p);
