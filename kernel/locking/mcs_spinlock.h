4986e775dc9abe0f634aa3b7c69002df7dfa3e4d
Davidlohr Bueso <dave@stgolabs.net>
2015-01-06 11:45:07 -0800

    locking/mcs: Better differentiate between MCS variants
    We have two flavors of the MCS spinlock: standard and cancelable (OSQ).
    While each one is independent of the other, we currently mix and match
    them. This patch:
    
      - Moves the OSQ code out of mcs_spinlock.h (which only deals with the traditional
        version) into include/linux/osq_lock.h. No unnecessary code is added to the
        more global header file, anything locks that make use of OSQ must include
        it anyway.
    
      - Renames mcs_spinlock.c to osq_lock.c. This file only contains osq code.
    
      - Introduces a CONFIG_LOCK_SPIN_ON_OWNER in order to only build osq_lock
        if there is support for it.
    
    Signed-off-by: Davidlohr Bueso <dbueso@suse.de>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: "Paul E. McKenney" <paulmck@linux.vnet.ibm.com>
    Cc: Jason Low <jason.low2@hp.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Mikulas Patocka <mpatocka@redhat.com>
    Cc: Waiman Long <Waiman.Long@hp.com>
    Link: http://lkml.kernel.org/r/1420573509-24774-5-git-send-email-dave@stgolabs.net
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/locking/mcs_spinlock.h
+++ kernel/locking/mcs_spinlock.h
@@ -99,20 +99,4 @@ void mcs_spin_unlock(struct mcs_spinlock **lock, struct mcs_spinlock *node)
 	smp_store_release(&next->locked, 1);
 }
 
-/*
- * Cancellable version of the MCS lock above.
- *
- * Intended for adaptive spinning of sleeping locks:
- * mutex_lock()/rwsem_down_{read,write}() etc.
- */
-
-struct optimistic_spin_node {
-	struct optimistic_spin_node *next, *prev;
-	int locked; /* 1 if lock acquired */
-	int cpu; /* encoded CPU # value */
-};
-
-extern bool osq_lock(struct optimistic_spin_queue *lock);
-extern void osq_unlock(struct optimistic_spin_queue *lock);
-
 #endif /* __LINUX_MCS_SPINLOCK_H */
