7edea7892da053e6668cbd2c15e223018751c0b2
Erik Kline <ek@google.com>
2015-07-22 16:38:25 +0900

    ipv6: sysctl to restrict candidate source addresses
    Per RFC 6724, section 4, "Candidate Source Addresses":
    
        It is RECOMMENDED that the candidate source addresses be the set
        of unicast addresses assigned to the interface that will be used
        to send to the destination (the "outgoing" interface).
    
    Add a sysctl to enable this behaviour.
    
    Signed-off-by: Erik Kline <ek@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    
    [Simplified back-port of net-next 3985e8a3611a93bb36789f65db862e5700aab65e]
    
    Bug: 19470192
    Bug: 21832279
    Bug: 22464419
    Change-Id: Icd96382f814a6f3ea53f05beb98c266b1929c5a3
    
    (cherry picked from commit c42896fa16eddc945433ab6cd66b50b455c9aa1c)

--- include/linux/ipv6.h
+++ include/linux/ipv6.h
@@ -175,6 +175,7 @@ struct ipv6_devconf {
 	__s32		force_tllao;
 	__s32		accept_ra_prefix_route;
 	__s32		accept_ra_mtu;
+	__s32		use_oif_addrs_only;
 	void		*sysctl;
 };
 
@@ -219,6 +220,7 @@ enum {
 	DEVCONF_ACCEPT_RA_PREFIX_ROUTE,
 	DEVCONF_ACCEPT_RA_RT_TABLE,
 	DEVCONF_ACCEPT_RA_MTU,
+	DEVCONF_USE_OIF_ADDRS_ONLY,
 	DEVCONF_MAX
 };
 
