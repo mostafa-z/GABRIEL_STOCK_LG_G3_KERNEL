4ccf0aec23997e30c6e9b8c493e3893c6f45f33b
Vinayak Menon <vinmenon@codeaurora.org>
2015-08-27 16:29:37 +0530

    lowmemorykiller: add zcache awareness
    zcache stores compressed file pages which should be
    considered as reclaimable pages by lowmemorykiller
    in calculating values to be compared against minfree.
    
    Change-Id: Ia3e08bc14ba61c0a45ed54ba5cd525717a572060
    Signed-off-by: Vinayak Menon <vinmenon@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -46,6 +46,7 @@
 #include <linux/cpuset.h>
 #include <linux/show_mem_notifier.h>
 #include <linux/vmpressure.h>
+#include <linux/zcache.h>
 
 #include <trace/events/memkill.h>
 #define CREATE_TRACE_POINTS
@@ -519,8 +520,8 @@ static unsigned long lowmem_scan(struct shrinker *s, struct shrink_control *sc)
 	other_free = global_page_state(NR_FREE_PAGES);
 
 	if (global_page_state(NR_SHMEM) + total_swapcache_pages() <
-		global_page_state(NR_FILE_PAGES))
-		other_file = global_page_state(NR_FILE_PAGES) -
+		global_page_state(NR_FILE_PAGES + zcache_pages())
+		other_file = global_page_state(NR_FILE_PAGES) + zcache_pages() -
 						global_page_state(NR_SHMEM) -
 						global_page_state(NR_UNEVICTABLE) -
 						total_swapcache_pages();
