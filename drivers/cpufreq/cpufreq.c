dd4790070bda141cb91964a8d08e46d245491aa8
mostafa-z <mostafazarghami@gmail.com>
2016-09-03 13:50:04 +0430

    cpufreq: Typecast compared integers
    The min/max frequencies in cpufreq_policy are unsigned integers
    but pm_qos_request() returns a signed int. Compare as unsigned,
    frequencies are never negative.
    
    Bug 949219
    
    Change-Id: Iba0de9ad6bf221f7a2e5560f597aa56cbeb7b6f6
    Signed-off-by: Antti P Miettinen <amiettinen@nvidia.com>
    Reviewed-on: http://git-master/r/91214
    Reviewed-by: Automatic_Commit_Validation_User
    Reviewed-by: Terje Bergstrom <tbergstrom@nvidia.com>
    Reviewed-by: Juha Tukkinen <jtukkinen@nvidia.com>
    
    Rebase-Id: Rf996a487f263e4bee451da38d3317a6ae53214dd

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -1982,10 +1982,14 @@ static int __cpufreq_set_policy(struct cpufreq_policy *data,
 {
 	int ret = 0;
 
+	unsigned int qmin, qmax;
 	unsigned int pmin = policy->min;
 	unsigned int pmax = policy->max;
-	unsigned int qmin = pm_qos_request(PM_QOS_CPU_FREQ_MIN);
-	unsigned int qmax = pm_qos_request(PM_QOS_CPU_FREQ_MAX);
+
+	qmin = min((unsigned int)pm_qos_request(PM_QOS_CPU_FREQ_MIN),
+		   data->max);
+	qmax = max((unsigned int)pm_qos_request(PM_QOS_CPU_FREQ_MAX),
+		   data->min);
 
 	pr_debug("setting new policy for CPU %u: %u - %u (%u - %u) kHz\n",
 		policy->cpu, pmin, pmax, qmin, qmax);
