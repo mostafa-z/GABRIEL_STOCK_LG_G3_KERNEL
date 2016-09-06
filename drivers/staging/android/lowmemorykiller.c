e63ca996b34e8afb8a9e45eb7a4e5c8b3cb4b747
mostafa-z <mostafazarghami@gmail.com>
2016-09-06 13:22:53 +0430

    CLEANUP AFTER MERGE
    (cherry picked from commit 29f71b393927d1e416c3fb13549d00865b53b079)

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -620,8 +620,6 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 		uid = pcred->uid;
 
 		if (avoid_to_kill(uid) || protected_apps(p->comm)) {
-#else
-		if (avoid_to_kill(uid)) {
 			if (tasksize * (long)(PAGE_SIZE / 1024) >= 50000) {
 				selected = p;
 				selected_tasksize = tasksize;
