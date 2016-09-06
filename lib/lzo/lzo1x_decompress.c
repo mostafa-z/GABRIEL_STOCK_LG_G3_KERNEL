460eee0755035fb8b9da2a9646b5abf5e17c8208
mostafa-z <mostafazarghami@gmail.com>
2016-09-06 14:38:34 +0430

    CLEANUP AFTER MERGE
    (cherry picked from commit ebe17048d766fa682e94aba51ccca6ce2cb548f5)

--- lib/lzo/lzo1x_decompress.c
+++ lib/lzo/lzo1x_decompress.c
@@ -23,10 +23,10 @@
 #define HAVE_IP(x, ip_end, ip) ((size_t)(ip_end - ip) < (x))
 #define HAVE_OP(x, op_end, op) ((size_t)(op_end - op) < (x))
 #define HAVE_LB(m_pos, out, op) (m_pos < out || m_pos >= op)
-
+#if 0
 #define COPY4(dst, src)	\
 		put_unaligned(get_unaligned((const u32 *)(src)), (u32 *)(dst))
-
+#endif
 int lzo1x_decompress_safe(const unsigned char *in, size_t in_len,
 			unsigned char *out, size_t *out_len)
 {
