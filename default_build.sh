791ed2196f5fe9484943e32e7fa025078c0f08e1
mostafa-z <mostafazarghami@gmail.com>
2016-09-28 23:58:28 +0330

    BUILDER: add GCC 6.1 support

--- default_build.sh
+++ default_build.sh
@@ -26,6 +26,7 @@ TCUB600=(TOOLCHAIN/UBERTC-6.0/bin/arm-eabi-);
 TCUB700=(TOOLCHAIN/UBERTC-7.0/bin/arm-eabi-);
 TCDR530=(TOOLCHAIN/TC-5.3-Dorimanx/bin/arm-eabi-);
 TCDR540=(TOOLCHAIN/TC-5.4-Dorimanx/bin/arm-eabi-);
+TCDR610=(TOOLCHAIN/TC-6.1-Dorimanx/bin/arm-eabi-);
 TCLN494=(TOOLCHAIN/linaro-4.9.4-dorimanx/bin/arm-LG-linux-gnueabi-);
 TCLN490=(TOOLCHAIN/linaro-4.9/bin/arm-eabi-);
 TCLN530=(TOOLCHAIN/linaro-5.3/bin/arm-eabi-);
@@ -321,7 +322,7 @@ fi;
 }
 
 echo "Select Toolchain ... ";
-select CHOICE in ARCHI-4.9.3 ARCHI-5.1.0 ARCHI-5.2.0 UBER-5.1.1 UBER-5.2.0 UBER-5.3.0 UBER-6.0.0 UBER-7.0.0 DORI-LINARO-4.9.4 DORI-5.3.X DORI-5.4.X LINARO-4.9.x LINARO-5.3.x LAST_ONE CLEANUP CONTINUE_BUILD; do
+select CHOICE in ARCHI-4.9.3 ARCHI-5.1.0 ARCHI-5.2.0 UBER-5.1.1 UBER-5.2.0 UBER-5.3.0 UBER-6.0.0 UBER-7.0.0 DORI-LINARO-4.9.4 DORI-5.3.X DORI-5.4.X DORI-6.1.X LINARO-4.9.x LINARO-5.3.x LAST_ONE CLEANUP CONTINUE_BUILD; do
 	case "$CHOICE" in
 		"ARCHI-4.9.3")
 			TC=$TCA493;
@@ -355,6 +356,9 @@ select CHOICE in ARCHI-4.9.3 ARCHI-5.1.0 ARCHI-5.2.0 UBER-5.1.1 UBER-5.2.0 UBER-
 			break;;
 		"DORI-5.4.X")
 			TC=$TCDR540;
+			break;;
+		"DORI-6.1.X")
+			TC=$TCDR610;
 			break;;
 		"LINARO-4.9.x")
 			TC=$TCLN490;
