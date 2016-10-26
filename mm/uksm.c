dd751260b8e207554944f5177f286b7d86e7e6ad
mostafa-z <mostafazarghami@gmail.com>
2016-10-26 17:14:55 +0330

    mm:uksm: fix compile error

--- mm/uksm.c
+++ mm/uksm.c
@@ -4680,8 +4680,7 @@ out:
 	return referenced;
 }
 
-int try_to_unmap_ksm(struct page *page,
-			enum ttu_flags flags, struct vm_area_struct *vma)
+int try_to_unmap_ksm(struct page *page, enum ttu_flags flags)
 {
 	struct stable_node *stable_node;
 	struct node_vma *node_vma;
