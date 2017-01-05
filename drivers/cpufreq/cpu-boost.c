03a5f6549194ba0ff61b7449a04b0bd008ae8079
mstafa-z <mostafazarghami@gmail.com>
2017-01-05 15:26:40 +0330

    cpufreq: cpu-boost: set interval between consecutive boosts original commit: https://github.com/franciscofranco/hammerhead/commit/1d15289389743fb9f54dc351b1664bc6bda4c6bb

--- drivers/cpufreq/cpu-boost.c
+++ drivers/cpufreq/cpu-boost.c
@@ -216,7 +216,7 @@ static void cpuboost_input_event(struct input_handle *handle,
 		return;
 
 	now = ktime_to_us(ktime_get());
-	if ((now - last_input_time) < (min_input_interval * USEC_PER_MSEC))
+	if ((now - last_input_time) < (input_boost_ms * USEC_PER_MSEC))
 		return;
 
 	if (work_pending(&input_boost_work))
