441fbf4696f621e3c0b6ff3e1565e964827222ac
mstafa-z <mostafazarghami@gmail.com>
2017-01-07 00:35:40 +0330

    Revert "rtmutex: Simplify remove_waiter()"
    This reverts commit 055bbe60d1b2d2bdcd8ef1b3888280c98662a2e9.

--- kernel/locking/rtmutex.c
+++ kernel/locking/rtmutex.c
@@ -894,9 +894,9 @@ static void wakeup_next_waiter(struct rt_mutex *lock)
 static void remove_waiter(struct rt_mutex *lock,
 			  struct rt_mutex_waiter *waiter)
 {
-	bool is_top_waiter = (waiter == rt_mutex_top_waiter(lock));
+	int first = (waiter == rt_mutex_top_waiter(lock));
 	struct task_struct *owner = rt_mutex_owner(lock);
-	struct rt_mutex *next_lock;
+	struct rt_mutex *next_lock = NULL;
 	unsigned long flags;
 
 	raw_spin_lock_irqsave(&current->pi_lock, flags);
@@ -904,31 +904,29 @@ static void remove_waiter(struct rt_mutex *lock,
 	current->pi_blocked_on = NULL;
 	raw_spin_unlock_irqrestore(&current->pi_lock, flags);
 
-	/*
-	 * Only update priority if the waiter was the highest priority
-	 * waiter of the lock and there is an owner to update.
-	 */
-	if (!owner || !is_top_waiter)
+	if (!owner)
 		return;
 
-	raw_spin_lock_irqsave(&owner->pi_lock, flags);
+	if (first) {
 
-	rt_mutex_dequeue_pi(owner, waiter);
+		raw_spin_lock_irqsave(&owner->pi_lock, flags);
 
-	if (rt_mutex_has_waiters(lock))
-		rt_mutex_enqueue_pi(owner, rt_mutex_top_waiter(lock));
+		rt_mutex_dequeue_pi(owner, waiter);
 
-	__rt_mutex_adjust_prio(owner);
+		if (rt_mutex_has_waiters(lock)) {
+			struct rt_mutex_waiter *next;
 
-	/* Store the lock on which owner is blocked or NULL */
-	next_lock = task_blocked_on_lock(owner);
+			next = rt_mutex_top_waiter(lock);
+			rt_mutex_enqueue_pi(owner, next);
+		}
+		__rt_mutex_adjust_prio(owner);
 
-	raw_spin_unlock_irqrestore(&owner->pi_lock, flags);
+		/* Store the lock on which owner is blocked or NULL */
+		next_lock = task_blocked_on_lock(owner);
 
-	/*
-	 * Don't walk the chain, if the owner task is not blocked
-	 * itself.
-	 */
+		raw_spin_unlock_irqrestore(&owner->pi_lock, flags);
+	}
+
 	if (!next_lock)
 		return;
 
