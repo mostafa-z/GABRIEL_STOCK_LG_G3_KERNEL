925b8564be3da5784c12834a1e8f3ba18db02a91
mostafa-z <mostafazarghami@gmail.com>
2017-05-25 15:36:56 +0430

    THERMAL: drivers breaks easily under heat... here is the temp log after rom installation: cat /sys/class/thermal/thermal_zone5/temp : 76~79 cat /sys/class/thermal/thermal_zone0/temp : 69~70 + it varies between 5~10 degree - after some investigation even the lg stock settings use sensor_5 but other apps use sensor_0 for cpu temp monitor, so it can easily produce lag and freq & core control by thermal driver. - stock driver has this config: 	qcom,limit-temp = <60>; 	qcom,temp-hysteresis = <10>; 	qcom,therm-reset-temp = <115>; 	qcom,freq-step = <2>; 	qcom,core-limit-temp = <80>; once i saw system critical temp shutdown message, so if i want give some power to it just raising limit-temp to around 70.

--- drivers/thermal/msm_thermal.c
+++ drivers/thermal/msm_thermal.c
@@ -73,14 +73,14 @@ static uint32_t hist_index = 0;
 static struct msm_thermal_data msm_thermal_info;
 
 static struct msm_thermal_data_intelli msm_thermal_info_local = {
-	.sensor_id = 5,
+	.sensor_id = 0,
 	.poll_ms = DEFAULT_POLLING_MS,
-	.limit_temp_degC = 75,
-	.temp_hysteresis_degC = 5,
+	.limit_temp_degC = 70,
+	.temp_hysteresis_degC = 10,
 	.freq_step = 2,
 	.freq_control_mask = 0xf,
-	.core_limit_temp_degC = 85,
-	.core_temp_hysteresis_degC = 5,
+	.core_limit_temp_degC = 80,
+	.core_temp_hysteresis_degC = 10,
 	.core_control_mask = 0xe,
 };
 
