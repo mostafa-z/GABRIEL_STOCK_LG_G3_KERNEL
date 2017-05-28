MotorolaMobilityLLC/kernel-msm

sched: Make RT tasks eligible for boost
During sched boost RT tasks currently end up going to the lowest
power cluster. This can be a performance bottleneck especially if
the frequency and IPC differences between clusters are high.
Furthermore, when RT tasks go over to the little cluster during
boost, the load balancer keeps attempting to pull work over to the
big cluster. This results in pre-emption of the executing RT task
causing more delays. Finally, containing more work on a single
cluster during boost might help save some power if the little
cluster can then enter deeper low power modes.

Change-Id: I177b2e81be5657c23e7ac43889472561ce9993a9
Signed-off-by: Syed Rameez Mustafa <rameezmustafa@codeaurora.org>

kernel/sched/rt.c
@@ -1547,6 +1547,10 @@ static int find_lowest_rq_hmp(struct task_struct *task)
 		cpu_cost = power_cost_at_freq(i, ACCESS_ONCE(rq->min_freq));
 		trace_sched_cpu_load(rq, idle_cpu(i),
 				     mostly_idle_cpu(i), cpu_cost);
+
+		if (sched_boost() && capacity(rq) != max_capacity)
+			continue;
+
 		if (cpu_cost < min_cost) {
 			min_cost = cpu_cost;
 			best_cpu = i;

mostafa-z/GABRIEL_STOCK_LG_G3_KERNEL

kernel/sched/rt.c
@@ -1543,6 +1543,10 @@ static int find_lowest_rq_hmp(struct task_struct *task)
 		cpu_cost = power_cost_at_freq(i, ACCESS_ONCE(rq->min_freq));
 		trace_sched_cpu_load(rq, idle_cpu(i),
 				     mostly_idle_cpu(i), cpu_cost);
+
+		if (sched_boost() && cpu_capacity(i) != max_capacity)
+			continue;
+
 		if (cpu_cost < min_cost && !sched_cpu_high_irqload(i)) {
 			min_cost = cpu_cost;
 			best_cpu = i;