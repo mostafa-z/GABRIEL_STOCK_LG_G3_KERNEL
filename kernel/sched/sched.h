7b13b426b600a7cdaa0a78c7c4a9fa33dce43c7a
mostafa-z <mostafazarghami@gmail.com>
2017-05-28 12:32:21 +0430

    Revert "Revert "sched_avg: add run queue averaging.""
    This reverts commit ff092e99218dc31f1fdee5071b9dd2f8573fd04c.

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1604,6 +1604,7 @@ static inline void inc_nr_running(struct rq *rq)
 #if defined(CONFIG_INTELLI_HOTPLUG) || defined(CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
 	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
 #endif
+	sched_update_nr_prod(cpu_of(rq), rq->nr_running, true);
 #if defined(CONFIG_INTELLI_HOTPLUG) || defined(CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
 	write_seqcount_begin(&nr_stats->ave_seqcnt);
 	nr_stats->ave_nr_running = do_avg_nr_running(rq);
@@ -1625,6 +1626,7 @@ static inline void dec_nr_running(struct rq *rq)
 #if defined(CONFIG_INTELLI_HOTPLUG) || defined(CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
 	struct nr_stats_s *nr_stats = &per_cpu(runqueue_stats, rq->cpu);
 #endif
+	sched_update_nr_prod(cpu_of(rq), rq->nr_running, false);
 #if defined(CONFIG_INTELLI_HOTPLUG) || defined(CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
 	write_seqcount_begin(&nr_stats->ave_seqcnt);
 	nr_stats->ave_nr_running = do_avg_nr_running(rq);
